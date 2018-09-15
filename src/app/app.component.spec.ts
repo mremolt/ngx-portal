4eb29b36cfd61b2c4310cf97c7d126304e6c0016
Marc Remolt <marc.remolt@gmail.com>
2018-09-15 23:38:14 +0200

    Test refactorings

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 
@@ -8,6 +8,8 @@ import { configureTestBedSuite } from '../testing';
 describe('AppComponent', () => {
   configureTestBedSuite();
 
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeAll(async () => {
     TestBed.configureTestingModule({
       imports: [RouterTestingModule],
@@ -16,23 +18,28 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+  });
+
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'ngx-portal'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('NGX-Portal');
   });
 
   it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-
     expect(compiled.querySelector('h1').textContent).toContain('Welcome to NGX-Portal!');
+  });
+
+  it('renders the component', () => {
+    expect(fixture).toMatchSnapshot();
   });
 });
